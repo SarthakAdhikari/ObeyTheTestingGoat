cd6919fd82ba3c2aca263c4959d9761eb4e153fd
SarthakAdhikari <adhikari.sarthak@gmail.com>
2020-01-09 17:11:46 +0545

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
-from selenium import webdriver
+import time
 import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
     
@@ -17,19 +20,27 @@ class NewVisitorTest(unittest.TestCase):
 
         #He notices the page title mention "To-do"
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         #He is invited to enter a to-do item straight away!
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         #He types "Buy peacock feathers" into a text box    
-
+        inputbox.send_keys('Buy peacock feathers')
         #When he hits enter, the page updates and the page lists:
-        # "1: Buy a peacock feathers" as an item in a to-do list
-
+        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
     
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
         #There is still a test box inviting him to add another item.
         #He enters "Use peacock feathers to make a fly"
 
+        self.fail('Finish the test!')
         #The page updates again and now shows both item on his 			list.             Then he sees that the site has generated a 			unique url for him             -- there is some explanatory 		text to that effect
 
         #He visits that URL- his to-do list is still there
